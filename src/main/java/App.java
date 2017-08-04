8d12c6815bc4f0336a161f71bb25313f486a0fe8
Kimberly Lu <kimlu4@comcast.net>
2017-08-03 21:26:52 -0700

    add method to calculate cost to ship package.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,31 +8,38 @@ import java.util.ArrayList;
 public class App {
     public static void main(String[] args) {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Welcome to WeShipAnywhere.  Please answer the following questions to determine your package volume or shipment costs.");
+        boolean programRunning = true;
+        while(programRunning) {
+            try {
+                Parcels userPackage = new Parcels();
 
-        try {
+                System.out.println("What would you like to know about your package: the 'volume', 'shipment cost, or 'exit'?");
+                String navigationChoice = bufferedReader.readLine();
 
-            ArrayList<Parcels> allPackages = new ArrayList<Parcels>();
+                if (navigationChoice.equals("volume")) {
+                    System.out.println("What is the length of your package?");
+                    int userInputL = Integer.parseInt(bufferedReader.readLine());
+                    System.out.println("What is the width of your package?");
+                    int userInputW = Integer.parseInt(bufferedReader.readLine());
+                    System.out.println("What is the height of your package?");
+                    int userInputH = Integer.parseInt(bufferedReader.readLine());
+                    int volume = userPackage.calcVolume(userInputL, userInputW, userInputH);
+                    System.out.println("Here is the volume of your package:" + volume);
+                }
+                if (navigationChoice.equals("shipment cost")) {
+                    System.out.println("Enter the weight of your package in pounds.");
+                    int userInputWt = Integer.parseInt(bufferedReader.readLine());
+                    int shipmentCost = userPackage.costToShip(userInputWt);
+                    System.out.println("Here is the cost to ship your package: $" + shipmentCost);
 
-            System.out.println("What is the length of your package?");
-            int userInputL = Integer.parseInt(bufferedReader.readLine());
-            System.out.println("What is the width of your package?");
-            int userInputW = Integer.parseInt(bufferedReader.readLine());
-            System.out.println("What is the height of your package?");
-            int userInputH = Integer.parseInt(bufferedReader.readLine());
-            System.out.println("What is the weight of your package in pounds?");
-            int userInputWt = Integer.parseInt(bufferedReader.readLine());
-
-            Parcels userPackage = new Parcels(userInputL, userInputW, userInputH, userInputWt);
-            allPackages.add(userPackage);
-
-            int volume = userPackage.volume(userInputL, userInputW, userInputH);
-            System.out.println("Here is the volume of your package:" + volume);
-            System.out.println("Based on your inputs, here is the cost to ship your package:");
-        }
-
-        catch(IOException e)
-        {
-            e.printStackTrace();
+                } else if (navigationChoice.equals("exit")) {
+                    System.out.println("Thank you for visiting our site");
+                    programRunning = false;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
     }
--- src/main/java/packages/Parcels.java
+++ src/main/java/packages/Parcels.java
@@ -1,18 +1,37 @@
 package packages;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Parcels {
     public int L;
     public int W;
     public int H;
     public int Weight;
+    public int cost;
 
-    public Parcels(int L, int W, int H, int Weight) {
-        this.L = L;
-        this.W = W;
-        this.H = H;
-        this.Weight = Weight;
+    Map<String, Integer> costByWt = new HashMap<String, Integer>();
+
+    public Parcels() {
+        costByWt.put("low", 5);
+        costByWt.put("mid", 10);
+        costByWt.put("high", 20);
     }
-    public int volume(int L, int W, int H) {
+    public int calcVolume(int L, int W, int H) {
         return (L * W * H);
+    }
+
+    public int costToShip(int Weight) {
+
+        if(Weight <= 1) {
+            cost = costByWt.get("low");
+        } else if(Weight >1 && Weight < 10) {
+            cost = costByWt.get("mid");
+        } else if(Weight >= 10) {
+            cost = costByWt.get("high");
+        } else {
+            System.out.println("Please enter a valid weight.");
+        }
+        return cost;
     }
 }
